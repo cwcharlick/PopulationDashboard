02c9e8b283271345c25ef7067ccaf31da8f13337
Chris Charlick <cwcharlick@outlook.com>
2022-12-02 11:34:39 +0000

    [Update] update text on About page to better describe the project

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,51 +7,69 @@ export const About = () => {
         <img src={Chris} alt="Chris" className="chris" />
         <p>Hello &#038; welcome to my custom dashboard project!</p>
         <p>
-          I'm a firm believer in control. It's always quicker to get up and
-          running with a high level option (such as Tableau), but for business
-          critical tools which affect daily productivity of an entire team it's
-          simply more important to "get it right" than to "get it quickly".
-        </p>
-        <p>
-          These dashboards are mainly built with Javascript, React.JS and D3.JS.
-          Importantly this allows complete control over every aspect. There are
-          a couple of different types of dashboard, so I've displayed 2 here.
+          I'm a big fan of data, so I thought it'd be fun to have a play around
+          with <b>D3</b>. I learnt a lot with this little project, see below for
+          the core bits to look out for on each page.
         </p>
       </div>
+      <h2>Top Level</h2>
+      <ul>
+        <li>
+          <b>React.JS</b>
+        </li>
+        <li>
+          <b>D3.JS</b>
+        </li>
+        <li>
+          <b>React Router</b>
+        </li>
+        <li>
+          <b>useState, useEffect</b> used throughout
+        </li>
+        <li>
+          <b>useContext</b> for app state (mainly for mouseover tooltips)
+        </li>
+
+        <li>
+          <b>Other basic notables:</b> Array methods (reduce, find, filter
+          amongst others). Object destructuring.
+        </li>
+      </ul>
       <h2>Global Population</h2>
+      <ul>
+        <li>Use the slider in the top left to zoom in & out</li>
+        <li>
+          <b>SVG manipulation & animation</b>
+        </li>
+        <li>
+          <b>Tooltips</b> - hover over stuff!
+        </li>
+        <li>
+          <b>useMemo</b>, graph calculations were too expensive to recalculate
+          for every tooltip state update (moving with the mouse), causing the
+          tooltip movement to be jerky. useMemo allowed me to memoise the
+          results so the tooltips can keep up with mouse movements.
+        </li>
+        <li>
+          <b>Filter component</b>. The year scale along the bottom filters the
+          graph & insights, which then animate to update. Mouseover to filter,
+          click to lock in a year after mousing out.
+        </li>
+      </ul>
+      <h2>Customer Success</h2>
       <p>
-        The first shows some visualisations and KPI style stats on Global
-        Population. This is merely a technical demo of the benefits of using D3
-        + React over high level tools such as Tableau. You have complete control
-        over components and interactivity, including animations (which can help
-        readability) and any kind of custom filters.
-      </p>
-      <p>
-        Mouse over to preview, or click to lock in, a different year from the
-        year-filter component at the bottom to instantly see the entire
-        dashboard's visuals and KPIs update.
+        Just playing around with other visuals in D3. Seeing what could be done.
       </p>
       <h2>Touch Plan</h2>
       <p>
-        Secondly we have a completely different type of "dashboard" which I
-        refer to as a "tool". Highly Customer Success specific, the Touch Plan
-        creates a kind of dashboard / report which Customer Success Managers can
-        use to keep on top of their proactive outreach.
-      </p>
-      <p>
-        It pulls account and activity (Event and Task) data from a CRM. From
-        these we can see which specific accounts are due, or overdue, and even
-        the type of outreach they need (Eg. within first 90 days? They need a
-        second or third on-boarding catch out. Outside the first 90 days?
-        They're due a QBR.)
-      </p>
-      <p>
-        The Touch Plan can either (or both) update automatically from new
-        activities logged in the CRM, or can even allow the CSMs to record their
-        activity here and it will automatically update the CRM itself.
-      </p>
-      <p>
-        If you don't already use a touch plan, it's a game changer for CSMs.
+        A sort of activity tracker, with some KPIs. Each row can be "C, T or E".
+        "Contacted, Task or Event". If a row is "contacted" or nothing at all
+        selected then its "Task or Event" is overdue.
+        <p>
+          Different rows are assigned to different people, simple filter
+          component at the top, and updating each row to "C, T or E" updates the
+          visual and KPI at the top.
+        </p>
       </p>
     </div>
   );
